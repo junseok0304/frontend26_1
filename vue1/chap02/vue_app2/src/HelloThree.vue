c7e8385e739e153bf59564cfa95ad15da13b1e49
junseok0304 <wicaduce@gmail.com>
2026-05-11 19:51:25 +0900

    02Vue_project

---
+++ vue1/chap02/vue_app2/src/HelloThree.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+const studentNo = ref('202214043');
+const name = ref('홍길동');
+</script>
+
+<template>
+    <div>
+        <p>학번: {{ studentNo }}</p>
+        <input type="text" v-model="studentNo">
+        <p>이름: {{ name }}</p>
+        <input type="text" v-model="name">
+    </div>
+</template>
+
+<style scoped>
+    div { 
+        background-color: #f0f0f0; padding: 1em;
+        text-align: center;
+    }
+</style>
---
+++ vue1/chap02/vue_app2/src/HelloTwo.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const message = ref('좋은 아침!');
+console.log(message.value);
+</script>
+
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <input type="text" v-model="message">
+    </div>
+</template>
+
+<style scoped> 
+    div { background-color: #f8f8f8; padding: 1em; text-align: center; }
+    input { padding: 4px; }
+</style>
