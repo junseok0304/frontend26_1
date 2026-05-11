eba5060fbd344f070af2d8f57352b477880639c6
junseok0304 <wicaduce@gmail.com>
2026-05-11 19:58:20 +0900

    03Vue_project:radio버튼구현

---
+++ vue1/chap03/vue_app3/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyForm from './MyForm.vue';
+</script>
+
+<template>
+  <main class="app">
+    <MyForm />
+  </main>
+</template>
+
+<style scoped>
+main.app { 
+  width: 500px; min-height: 300px;
+  margin: 1em auto; padding: 1em;
+  border: 1px solid gray;
+  box-shadow: 3px 3px 3px #ddd;
+}
+</style>
---
+++ vue1/chap03/vue_app3/src/MyForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+const enabled = ref(true);
+const gender = ref("남");
+</script>
+
+<template>
+    <div class="MyForm">
+        <div class="row">
+            <label><input type="checkbox" v-model="enabled" /> 활성</label>
+        </div>
+        <div class="row">
+            <label><input type="radio" value="남" v-model="gender" /></label>
+            <label><input type="radio" value="여" v-model="gender" /></label>
+        </div>
+        <hr />
+        <div class="row">활성: {{ enabled }}</div>
+        <div class="row">성별: {{ gender }}</div>
+    </div>
+</template>
+
+<style scoped>
+div.MyForm { padding: 2em; background: #f8f8f8; }
+div.row { margin: 0.7em; }
+label:has(input[type="radio"]) { margin-right: 1em; }
+</style>
