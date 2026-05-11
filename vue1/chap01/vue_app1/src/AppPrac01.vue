f1013fe44c5848cad286b043036d83b706387717
junseok0304 <wicaduce@gmail.com>
2026-05-11 14:43:44 +0900

    01Vue_project

---
+++ vue1/chap01/vue_app1/src/AppPrac01.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>안녕하세요.</h1>
+    <p>첫 번째 Vue.js App 입니다.</p>
+  </main>
+</template>
+
+<style scoped>
+main { padding: 30px; /* 상하좌우 내부 여백 30픽셀 */
+  margin: 30px auto; /* 상하 외부 여백 30픽셀에 좌우 외부 여백 : 중앙정렬로 */
+  text-align: center; /* 텍스트를 중앙 정렬로 설정 */
+  max-width: 400px; /* 최대 너비를 400픽셀로 설정 */ 
+  box-shadow: 3px 3px 8px #aaa; /* 그림자 3픽셀 오른쪽 아래쪽 블러, 회색으로 설정 */ }
+p { color: blue; /* 글자 색상을 파란색으로 설정 */ }
+</style>
---
+++ vue1/chap01/vue_app1/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './AppPrac01.vue'
+
+createApp(App).mount('#app')
