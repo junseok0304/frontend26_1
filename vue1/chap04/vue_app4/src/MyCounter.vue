9ac8f2290a50ff08f2d85aaaad6de9898a8d05f0
junseok0304 <wicaduce@gmail.com>
2026-05-12 10:35:23 +0900

    04Vue_project:props값 디폴트  구현

--- vue1/chap04/vue_app4/src/MyCounter.vue
+++ vue1/chap04/vue_app4/src/MyCounter.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-const {title, step} = defineProps(['title','step']);
+const {title="카운터", step=1} = defineProps(['title','step']);
 //defineProps(['title', 'step']); template에서만 사용시
 const count = ref(0);
 console.log(title,step);
