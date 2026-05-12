aca66fc98bad81c3e83847f6b6f2b3b00f71515a
junseok0304 <wicaduce@gmail.com>
2026-05-12 09:59:46 +0900

    04Vue_project:props v-on:click 구현

---
+++ vue1/chap04/vue_app4/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyCounter from './MyCounter.vue'
+</script>
+
+<template>
+  <main class="app">
+    <MyCounter/>
+  </main>
+</template>
+
+<style scoped>
+main.app { 
+  width: 400px; min-height: 200px;
+  margin: 1em auto; padding: 1em;
+  border: 1px solid gray;
+  box-shadow: 3px 3px 3px #ddd;
+}
+</style>
---
+++ vue1/chap04/vue_app4/src/MyCounter.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <p> {{ count }}</p>
+        <button type="button" v-on:click="++count">Increase</button>
+        <button type="button" v-on:click="--count">Decrease</button>
+    </div>
+</template>
+
+<style scoped>
+div { padding: 1em; text-align: center; 
+background-color: #e8ffe8; border: 1px solid #aca; }
+p { font-size: 20pt; text-align: center; margin: 0; }
+button { padding: 0.7em 1.5em; margin: 5px; }
+</style>
