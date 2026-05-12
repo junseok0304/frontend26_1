0bbff695b7db1c8b76ea10d9f76c35071d66f295
junseok0304 <wicaduce@gmail.com>
2026-05-12 13:21:16 +0900

    05Vue_project: v-show v-if

---
+++ vue1/chap05/vue_app5/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MyCheckbox1 from './MyCheckbox1.vue';
+import MyCheckbox2 from './MyCheckbox2.vue';
+</script>
+
+<template>
+  <main class="app">
+    <MyCheckbox1 />
+    <hr />
+    <MyCheckbox2 />
+  </main>
+</template>
+
+<style scoped>
+main.app {
+  width: 400px;
+  margin: 1em auto;
+  padding: 1em;
+  border: 1px solid gray;
+  box-shadow: 3px 3px 3px #ddd;
+}
+</style>
