23357df075a0d77a9efa82cc4e27e4cfc84f09ad
junseok0304 <wicaduce@gmail.com>
2026-05-12 15:41:14 +0900

    06Vue_project: v-for 지시자

---
+++ vue1/chap06/vue_app6/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MyList1 from './MyList1';
+</script>
+
+<template>
+  <div id="app">
+    <MyList1 />
+  </div>
+</template>
+
+<style scoped>
+div#app {
+  padding: 30px;
+  margin: 30px auto;
+  width: 600px;
+  border: 1px solid #ccc;
+  box-shadow: 3px 3px 3px #ddd;
+}
+</style>
---
+++ vue1/chap06/vue_app6/src/MyList1.vue
@@ -0,0 +1,20 @@
+<script setup>
+const list = ["zero", "one", "two", "three", "four", "five"];
+</script>
+
+<template>
+    <div>
+        <span v-for="s in list" :key="s">
+            {{ s }}
+        </span>
+    </div>
+</template>
+
+<style scoped>
+span {
+    display: inline-block;
+    margin: 5px;
+    padding: 8px 15px;
+    border: 1px solid black;
+}
+</style>
