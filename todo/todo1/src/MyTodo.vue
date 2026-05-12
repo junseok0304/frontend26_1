63f4366f1789c464c597387b6e14c91527880a6b
junseok0304 <wicaduce@gmail.com>
2026-05-12 23:32:18 +0900

    todo1_project: todo app 추가기능 구현

--- todo/todo1/src/MyTodo.vue
+++ todo/todo1/src/MyTodo.vue
@@ -1,9 +1,17 @@
 <script setup>
-const todos = [
+import { ref } from 'vue';
+const todos = ref([
     { id: 1, title: "과제" },
     { id: 2, title: "시험공부" },
     { id: 3, title: "코딩훈련" }
-]
+]);
+const title = ref('');
+function addTodo() {
+    const lastId = todos.value.length == 0 ? 0 :
+        todos.value[todos.value.length - 1].id;
+    todos.value.push({ id: lastId + 1, title: title.value });
+    title.value = "";
+}
 </script>
 <template>
     <div>
@@ -22,11 +30,14 @@ const todos = [
                 </tr>
             </tbody>
         </table>
+        <input type="text" v-model.trim="title" />
+        <button type="button" v-on:click="addTodo">추가</button>
     </div>
 </template>
 <style scoped>
 h1 {
     border-bottom: 1px solid gray;
+    text-shadow: 1px 1px 2px #aaa;
 }
 
 table {
@@ -48,5 +59,28 @@ td {
 td:nth-child(1) {
     text-align: center;
     width: 30px;
+}
+
+input[type=text] {
+    padding: 0.3em;
+    margin-right: 0.3em;
+    width: 22em;
+    font-size: 11pt;
+}
+
+button {
+    padding: .2em .9em .3em .9em;
+    border: none;
+    font-size: 11pt;
+    background-color: #08f;
+    box-shadow: 2px 2px 2px #aaa;
+    color: white;
+    text-shadow: 1px 1px 2px #444;
+}
+
+button:active {
+    box-shadow: none;
+    transform: translateY(2px);
+    border: none;
 }
 </style>
