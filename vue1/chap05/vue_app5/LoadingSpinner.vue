154920c1b08c77ac8c2b76ac364fa6edc821f713
junseok0304 <wicaduce@gmail.com>
2026-05-12 13:39:42 +0900

    05Vue_project: animation3

--- vue1/chap05/vue_app5/LoadingSpinner.vue
+++ vue1/chap05/vue_app5/LoadingSpinner.vue
@@ -8,15 +8,27 @@
 <style scoped>
 div.spinner {
     display: inline-block;
-    padding: 0; margin: 0 0.4em;
-    width: 1.5em; height: 1.5em;
+    padding: 0;
+    margin: 0 0.4em;
+    width: 1.5em;
+    height: 1.5em;
     border: 0.2em solid #ddd;
     border-top: 0.2em solid #88f;
     border-radius: 50%;
-    animation: spin-animation 1s linear infinite;
+    animation: spin-animation 1s cubic-bezier(0.2, 0, 0.8, 1) infinite;
 }
+
 @keyframes spin-animation {
-    from { transform: rotate(0deg); }
-    to { transform: rotate(360deg); }
+    from {
+        transform: rotate(0deg);
+    }
+
+    50% {
+        transform: rotate(180deg) scale(1.3);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
 }
 </style>
