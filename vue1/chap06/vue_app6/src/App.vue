d061c2e5d3c837f4883bb6765046fa9a6a72a7a3
junseok0304 <wicaduce@gmail.com>
2026-05-12 16:55:01 +0900

    06Vue_project: MyList2 구현

--- vue1/chap06/vue_app6/src/App.vue
+++ vue1/chap06/vue_app6/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import MyList1 from './MyList1.vue';
+import MyList2 from './MyList2.vue';
 </script>
 
 <template>
   <div id="app">
     <MyList1 />
+    <hr /> 
+    <MyList2 />
   </div>
 </template>
 
---
+++ vue1/chap06/vue_app6/src/MyList2.vue
@@ -0,0 +1,35 @@
+<script setup>
+const persons = [{ id: 101, name: "홍길동", age: 16 }, { id: 102, name: "김철수", age: 20 }, { id: 103, name: "박영희", age: 25 }, { id: 104, name: "최민수", age: 30 }];
+</script>
+
+<template>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>이름</td>
+            <td>나이</td>
+        </tr>
+        <tr v-for="person in persons" v-bind:key="person.id">
+            <td>{{ person.id }}</td>
+            <td>{{ person.name }}</td>
+            <td>{{ person.age }}</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+table {
+    border-collapse: collapse;
+    width: 400px;
+}
+
+td {
+    padding: 5px;
+    border: 1px solid #aaa;
+    text-align: center;
+}
+
+tr:nth-child(1) {
+    background-color: #ddd;
+}
+</style>
